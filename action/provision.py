342c60e255319e7443bc6885c3db776131115080
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2022-01-31 15:52:02 +0100

    fixed name of args

--- action/provision.py
+++ action/provision.py
@@ -5,22 +5,19 @@ from action.package import package
 
 
 @click.command()
-@click.option("--stack-name", help="Stack name")
-@click.option("--path", help="Path to the folder with CFN templates to package")
-@click.option("--main-template", help="Name of the main template file")
-@click.option("--branch", help="Branch name")
-@click.option("--artifacts-bucket", help="S3 bucket for artifacts")
-@click.option("--cfn-bucket", help="S3 bucket for cfn templates")
+@click.option("--stack-name", help="Stack name", required=True)
+@click.option("--path", help="Path to the folder with CFN templates to package", required=True)
+@click.option("--main-template", help="Name of the main template file", required=True)
+@click.option("--branch", help="Branch name", required=True)
+@click.option("--artifacts-bucket", help="S3 bucket for artifacts", required=True)
+@click.option("--cfn-bucket", help="S3 bucket for cfn templates", required=True)
 def provision(stack_name, path, main_template, branch, artifacts_bucket, cfn_bucket):
-    if not artifacts_bucket or not cfn_bucket:
-        raise ValueError("artifacts and cfn bucket names must be set")
-
     click.echo(f"{'='*15} Package {'='*15}")
     package(stack_name, path, main_template, branch, artifacts_bucket, cfn_bucket)
 
     click.echo(f"{'=' * 15} Deploy {'=' * 15}")
     stack_id = deploy(main_template, stack_name, branch)
-    click.echo(f"::set-output name=time::{stack_id}")
+    click.echo(f"::set-output name=stack_id::{stack_id}")
 
 
 if __name__ == "__main__":
